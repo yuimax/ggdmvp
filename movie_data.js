820073168cd26d26b5b43027847f2cfe712593d1
yuimax <31821337+yuimax@users.noreply.github.com>
2021-10-18 11:39:54 +0900

    Update movie_data.js

--- movie_data.js
+++ movie_data.js
@@ -1,5 +1,5 @@
 const movie_data = [
-'1J-JlQsc1IbLOiq6zyCTkwZnIM1eKL-Wj/29/STONE式MEIKOで「雲の遺跡」',
+'1J-JlQsc1IbLOiq6zyCTkwZnIM1eKL-Wj/--/STONE式MEIKOで「雲の遺跡」',
 '1qTdSX4HHlf6czya3SrXjkhK8JNhhIfBy/28/カウコさんが天の岩戸で「トキヲ・ファンカ」',
 '1O_aGIm99Jhef534Jdq0VW3yWVravQcLc/27/ウシ娘カウコさんで「ヒガン」',
 '1tOEBVVk22Om4QFZqtR9J-ZjNN9ypL0EO/26/もぶ太郎と姐さんたちで「ペリー来航」',
