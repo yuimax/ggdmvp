d34b6d276219fdb992f49e894e0a1d302ca66a30
yuimax <31821337+yuimax@users.noreply.github.com>
2022-05-18 10:14:36 +0900

    Update movie_data.js

--- movie_data.js
+++ movie_data.js
@@ -1,4 +1,5 @@
 const movie_data = [
+'1sQXTV6yxI2xz7DxhFA8JJTQPxuiZG90L/42/VRoidで「キライ・キライ・ジガヒダイ！」',
 '1AaMeeUqD4nL19Er2Obi2KBv3zy2S6GXk/41/VRoidで「恋愛裁判」',
 '1c5SwK_pAeh34OpAqX3GQnAJLuKOYSBDr/40/チンピラAで「シャンティ」',
 //'1rJmxKZ2yIaH-puM-eJIg8Tj3KgmoYSnI/39/ガリ子で「WAVEFILE」',
