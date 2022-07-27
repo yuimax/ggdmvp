9fd24fe48749529b6da1f4681cbe732e4cdff965
yuimax <31821337+yuimax@users.noreply.github.com>
2022-07-27 16:50:14 +0900

    Update movie_data.js

--- movie_data.js
+++ movie_data.js
@@ -29,7 +29,7 @@ const movie_data = [
 //'1T8g3sc00IXhnvPOe2uvMd5JTiYoUS0qt/12/AB10で「毒占欲」',
 //'124L6PF-JGWdLyn6S-m29NB39FHUFVyoA/10/水稀で「よくばり」',
 '1WkBX9Ml4zLxsIzGaSw8aNkOIgDrd7RDt/9/うに子で「他人事の音がする」',
-'1ptwp-j1f5zyy6VYPL33JZ5bd5pz6gUWC/7/アカガネで「キライ・キライ・ジガヒダイ！」',
+//'1ptwp-j1f5zyy6VYPL33JZ5bd5pz6gUWC/7/アカガネで「キライ・キライ・ジガヒダイ！」',
 //'10FgAiqk4XfXI7Nlg-B_hhBX8txK4ETCQ/6/アカガネで「少女ふぜゐ」',
 //'1KxgCWLekpgHJRq92g3AeQiYIU7giD_pV/3/葵乃と蒐華で「乱躁滅裂ガール」',
 //'1PK9jf70Gow7-londD7OiPj9MzNB4N1Ti/2/うに子と葵乃で「ロキ(みきとp)」',
