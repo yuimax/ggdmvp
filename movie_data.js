21994862059262778d4a6241aff31308c71bfd9e
yuimax <31821337+yuimax@users.noreply.github.com>
2022-08-14 01:43:37 +0900

    Update movie_data.js

--- movie_data.js
+++ movie_data.js
@@ -1,5 +1,5 @@
 const movie_data = [
-//'1kGxQJ9mxTAtO-LBsbTJGrAbuIwH9HVl_/--/ていらん式初音ミクで「神っぽいな」1080p',
+'1cJEVq_j8XIyk65S7lSjc2Oox27ucYUuN/45/近村麗愛で「Te_Amo」',
 '14iPIVqWVak4B3QE_0CquATOA0Kim2hN9/44/ていらん式初音ミクで「神っぽいな」',
 '1XYGaor3R2XJImvSoGqPi4nClKSDQwQOs/43/ふっこで「TheLastCrusade」',
 '1sQXTV6yxI2xz7DxhFA8JJTQPxuiZG90L/42/VRoidで「キライ・キライ・ジガヒダイ！」',
